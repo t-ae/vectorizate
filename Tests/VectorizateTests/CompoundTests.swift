4a48df3d243b9ccc07632077343d91bde26d6e6f
araki <araki@qoncept.co.jp>
2018-11-10 16:33:01 +0900

    Add tests

--- Tests/VectorizateTests/CompoundTests.swift
+++ Tests/VectorizateTests/CompoundTests.swift
@@ -8,9 +8,9 @@ class CompoundTests: XCTestCase {
     func testVMA() {
         do {
             typealias T = Float
-            let a: [T] = randomArray(count: count, in: 0..<1)
-            let b: [T] = randomArray(count: count, in: 0..<1)
-            let c: [T] = randomArray(count: count, in: 0..<1)
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b: [T] = randomArray(count: count, in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
             var ans1 = [T](repeating: 0, count: count)
             var ans2 = [T](repeating: 0, count: count)
             
@@ -21,9 +21,9 @@ class CompoundTests: XCTestCase {
         }
         do {
             typealias T = Double
-            let a: [T] = randomArray(count: count, in: 0..<1)
-            let b: [T] = randomArray(count: count, in: 0..<1)
-            let c: [T] = randomArray(count: count, in: 0..<1)
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b: [T] = randomArray(count: count, in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
             var ans1 = [T](repeating: 0, count: count)
             var ans2 = [T](repeating: 0, count: count)
             
@@ -37,22 +37,22 @@ class CompoundTests: XCTestCase {
     func testVMSb() {
         do {
             typealias T = Float
-            let a: [T] = randomArray(count: count, in: 0..<1)
-            let b: [T] = randomArray(count: count, in: 0..<1)
-            let c: [T] = randomArray(count: count, in: 0..<1)
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b: [T] = randomArray(count: count, in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
             var ans1 = [T](repeating: 0, count: count)
             var ans2 = [T](repeating: 0, count: count)
             
             Vectorizate.vmsb(a: a, ia: 1, b: b, ib: 1, c: c, ic: 1, out: &ans1, iOut: 1, count: count)
             VectorizateNoAccelerate.vmsb(a: a, ia: 1, b: b, ib: 1, c: c, ic: 1, out: &ans2, iOut: 1, count: count)
             
-            XCTAssertEqual(ans1, ans2, accuracy: 1e-4)
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-6)
         }
         do {
             typealias T = Double
-            let a: [T] = randomArray(count: count, in: 0..<1)
-            let b: [T] = randomArray(count: count, in: 0..<1)
-            let c: [T] = randomArray(count: count, in: 0..<1)
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b: [T] = randomArray(count: count, in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
             var ans1 = [T](repeating: 0, count: count)
             var ans2 = [T](repeating: 0, count: count)
             
@@ -60,6 +60,122 @@ class CompoundTests: XCTestCase {
             VectorizateNoAccelerate.vmsb(a: a, ia: 1, b: b, ib: 1, c: c, ic: 1, out: &ans2, iOut: 1, count: count)
             
             XCTAssertEqual(ans1, ans2)
+        }
+    }
+    
+    func testVMSA() {
+        do {
+            typealias T = Float
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b: [T] = randomArray(count: count, in: 1..<2)
+            let c = T.random(in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vmsa(a: a, ia: 1, b: b, ib: 1, c: c, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vmsa(a: a, ia: 1, b: b, ib: 1, c: c, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-6)
+        }
+        do {
+            typealias T = Double
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b: [T] = randomArray(count: count, in: 1..<2)
+            let c = T.random(in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vmsa(a: a, ia: 1, b: b, ib: 1, c: c, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vmsa(a: a, ia: 1, b: b, ib: 1, c: c, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2)
+        }
+    }
+    
+    func testVSMA() {
+        do {
+            typealias T = Float
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b = T.random(in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vsma(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vsma(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-6)
+        }
+        do {
+            typealias T = Double
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b = T.random(in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vsma(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vsma(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2)
+        }
+    }
+    
+    func testVSMSb() {
+        do {
+            typealias T = Float
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b = T.random(in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vsmsb(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vsmsb(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-6)
+        }
+        do {
+            typealias T = Double
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b = T.random(in: 1..<2)
+            let c: [T] = randomArray(count: count, in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vsmsb(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vsmsb(a: a, ia: 1, b: b, c: c, ic: 1, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-8)
+        }
+    }
+    
+    func testVSMSA() {
+        do {
+            typealias T = Float
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b = T.random(in: 1..<2)
+            let c = T.random(in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vsmsa(a: a, ia: 1, b: b, c: c, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vsmsa(a: a, ia: 1, b: b, c: c, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-6)
+        }
+        do {
+            typealias T = Double
+            let a: [T] = randomArray(count: count, in: 1..<2)
+            let b = T.random(in: 1..<2)
+            let c = T.random(in: 1..<2)
+            var ans1 = [T](repeating: 0, count: count)
+            var ans2 = [T](repeating: 0, count: count)
+            
+            Vectorizate.vsmsa(a: a, ia: 1, b: b, c: c, out: &ans1, iOut: 1, count: count)
+            VectorizateNoAccelerate.vsmsa(a: a, ia: 1, b: b, c: c, out: &ans2, iOut: 1, count: count)
+            
+            XCTAssertEqual(ans1, ans2, accuracy: 1e-12)
         }
     }
 }
